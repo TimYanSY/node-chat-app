b06a2161629d58665789ea33a3915850be6aca4f
TimYanSY <timyan@ece.neu.edu>
2018-01-30 16:34:59 -0800

    change port

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+  socket.on('disconnect', (reason) => {
+    console.log('user disconnect');
+  });
+})
+
+server.listen(port, () => {
   console.log(`You are listen to ${port}`)
 });
