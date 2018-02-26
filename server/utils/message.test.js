cd0330476baf7d951702837a36d9d34145494435
TimYanSY <timyan@ece.neu.edu>
2018-02-26 12:19:55 -0800

    create generate message utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,18 @@
+var expect = require('expect');
+
+var {generateMessage} = require('./message.js');
+
+describe('generateMessage', () => {
+  it('should generate correct message object', () => {
+    var from = "Admin";
+    var text = "Hello from Admin";
+    const message = generateMessage(from, text);
+
+    expect(message.createdAt).toBeA('number');
+
+    expect(message).toInclude({
+      from,
+      text
+    })
+  })
+});
